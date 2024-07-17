c6a46906db119248ce62d6e72d7d7eae44e6524f
SyedAnas-123 <syedmohammadanasahmed@gmail.com>
2024-07-18 01:53:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+//1 make object bcz user se sentence lengen 
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "ENTER YOUR SENTENCE TO COUNT THE WORD"
+    }
+]);
+//BCZ WE NEED WITHOUT SPACES SO USE THIS METHOD AND IT STORES IN ANOTHER VARIABLE
+//split converts it in array bcz we need sentences not alphabets and trim remove xtrxa spaces 
+const words = answers.Sentence.trim().split(" "); //answers ke varaible ke undr sentence ka jo object hai usme user ka unput aeega bus wo call krwan hai
+console.log(words);
+//NOW COUNT WORDS WHIHC ARE COMMINg IN ARRAY what is there length
+console.log(`YOUR SENTENCE WORD COUNT IS : ${words.length}`);
